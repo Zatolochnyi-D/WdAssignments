ac56b9433bca490bdb3ba70a82f248db0fdb0d22
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-11-20 15:27:24 +0200

    Added creation of test objects from first assignment 1.

--- MyndMapper/Program.cs
+++ MyndMapper/Program.cs
@@ -1,9 +1,39 @@
+using MyndMapper.Models;
+using MyndMapper.Objects;
+
 namespace MyndMapper;
 
 public class Program
 {
     public static void Main(string[] args)
     {
+        UserAccount.CreateUser(new()
+        {
+            Name = "Athos",
+            Email = "firstOne@nonexistentmail.com",
+            Password = "123456",
+        });
+        UserAccount.CreateUser(new()
+        {
+            Name = "Porthos",
+            Email = "secondOne@nonexistentmail.com",
+            Password = "654321",
+        });
+        UserAccount.CreateUser(new()
+        {
+            Name = "Aramis",
+            Email = "thirdOne@nonexistentmail.com",
+            Password = "123321",
+        });
+        CanvasModel model = new()
+        {
+            Name = "NewCanvas",
+            CreationDate = DateTime.Now,
+        };
+        model.SetOwnerId(0);
+        Canvas.CreateCanvas(model);
+        UserAccount.GetUserById(0).CreatedCanvases.Add(0);
+
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
