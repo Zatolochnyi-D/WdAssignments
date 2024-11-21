2931b6b198a690781e5ed939ec29f7c1c9c21c68
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-11-21 12:16:33 +0200

    Implemented controllers with new storages.

--- MyndMapper/Controllers/CanvasController.cs
+++ MyndMapper/Controllers/CanvasController.cs
@@ -1,117 +1,83 @@
 using Microsoft.AspNetCore.Mvc;
 using MyndMapper.Entities;
+using MyndMapper.Storages.CanvasStorage;
 
 namespace MyndMapper.Controllers;
 
 [ApiController]
 [Route("canvases/")]
-public class CanvasController : ControllerBase
+public class CanvasController(ICanvasStorage canvasStorage) : ControllerBase
 {
-    // [HttpPost("{creatorId}")]
-    // [ProducesResponseType(StatusCodes.Status200OK)]
-    // [ProducesResponseType(StatusCodes.Status404NotFound)]
-    // public ActionResult CreateCanvas(int creatorId, Canvas canvasModel)
-    // {
-    //     UserStorage user;
-    //     try
-    //     {
-    //         user = UserStorage.GetUserById(creatorId);
-    //     }
-    //     catch (ArgumentException)
-    //     {
-    //         return NotFound();
-    //     }
-    //     CanvasStorage canvas = CanvasStorage.CreateCanvas(creatorId, canvasModel);
-    //     user.CreatedCanvases.Add(canvas.Id);
-    //     return Ok();
-    // }
+    [HttpGet("canvasId={id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<Canvas> Get(int id)
+    {
+        Canvas canvas;
+        try
+        {
+            canvas = canvasStorage.Get(id);
+        }
+        catch (ArgumentException)
+        {
+            return NotFound();
+        }
+        return Ok(canvas);
+    }
 
-    // [HttpGet("canvasId={id}")]
-    // [ProducesResponseType(StatusCodes.Status200OK)]
-    // [ProducesResponseType(StatusCodes.Status404NotFound)]
-    // public ActionResult<Canvas> GetCanvas(int id)
-    // {
-    //     CanvasStorage canvas;
-    //     try
-    //     {
-    //         canvas = CanvasStorage.GetCanvasById(id);
-    //     }
-    //     catch (ArgumentException)
-    //     {
-    //         return NotFound();
-    //     }
-    //     return Canvas.CreateFromCanvas(canvas);
-    // }
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public ActionResult<Canvas[]> GetAll()
+    {
+        IEnumerable<Canvas> canvases = canvasStorage.GetAll();
+        return Ok(canvases);
+    }
 
-    // [HttpGet("userId={id}")]
-    // [ProducesResponseType(StatusCodes.Status200OK)]
-    // [ProducesResponseType(StatusCodes.Status404NotFound)]
-    // public ActionResult<Canvas[]> GetCanvasesOfUser(int id)
-    // {
-    //     UserStorage user;
-    //     try
-    //     {
-    //         user = Objects.UserStorage.GetUserById(id);
-    //     }
-    //     catch (ArgumentException)
-    //     {
-    //         return NotFound();
-    //     }
+    [HttpPost("{creatorId}")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult CreateCanvas(int creatorId, Canvas canvas)
+    {
+        try
+        {
+            canvasStorage.Create(creatorId, canvas);
+        }
+        catch (ArgumentException)
+        {
+            return NotFound();
+        }
+        return Created();
+    }
 
-    //     List<Canvas> canvasModels = [];
-    //     foreach (var canvasId in user.CreatedCanvases)
-    //     {
-    //         canvasModels.Add(Canvas.CreateFromCanvas(CanvasStorage.GetCanvasById(canvasId)));
-    //     }
-    //     return canvasModels.ToArray();
-    // }
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult EditCanvas(int id, Canvas canvas)
+    {
+        try
+        {
+            canvasStorage.Edit(id, canvas);
+        }
+        catch (ArgumentException)
+        {
+            return NotFound();
+        }
+        return NoContent();
+    }
 
-    // [HttpGet]
-    // [ProducesResponseType(StatusCodes.Status200OK)]
-    // public ActionResult<Canvas[]> GetAllCanvases()
-    // {
-    //     CanvasStorage[] canvases = CanvasStorage.GetAllCanvases();
-    //     List<Canvas> canvasModels = [];
-    //     foreach (var canvas in canvases)
-    //     {
-    //         canvasModels.Add(Canvas.CreateFromCanvas(canvas));
-    //     }
-    //     return canvasModels.ToArray();
-    // }
-
-    // [HttpPut("{id}")]
-    // [ProducesResponseType(StatusCodes.Status204NoContent)]
-    // [ProducesResponseType(StatusCodes.Status404NotFound)]
-    // public ActionResult EditCanvas(int id, Canvas canvasModel)
-    // {
-    //     CanvasStorage canvas;
-    //     try
-    //     {
-    //         canvas = CanvasStorage.GetCanvasById(id);
-    //     }
-    //     catch (ArgumentException)
-    //     {
-    //         return NotFound();
-    //     }
-    //     canvas.Update(canvasModel);
-    //     return NoContent();
-    // }
-
-    // [HttpDelete("{id}")]
-    // [ProducesResponseType(StatusCodes.Status204NoContent)]
-    // [ProducesResponseType(StatusCodes.Status404NotFound)]
-    // public ActionResult DeleteCanvasById(int id)
-    // {
-    //     try
-    //     {
-    //         CanvasStorage canvas = CanvasStorage.GetCanvasById(id);
-    //         Objects.UserStorage.GetUserById(canvas.OwnerId).CreatedCanvases.Remove(id);
-    //         CanvasStorage.DeleteCanvasById(id);
-    //         return NoContent();
-    //     }
-    //     catch (ArgumentException)
-    //     {
-    //         return NotFound();
-    //     }
-    // }
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult Remove(int id)
+    {
+        try
+        {
+            canvasStorage.Remove(id);
+        }
+        catch (ArgumentException)
+        {
+            return NotFound();
+        }
+        return NoContent();
+    }
 }
--- MyndMapper/Controllers/UserController.cs
+++ MyndMapper/Controllers/UserController.cs
@@ -1,86 +1,75 @@
 using Microsoft.AspNetCore.Mvc;
 using MyndMapper.Entities;
+using MyndMapper.Storages.UserStorage;
 
 namespace MyndMapper.Controllers;
 
 [ApiController]
 [Route("users/")]
-public class UserController : ControllerBase
+public class UserController(IUserStorage userStorage) : ControllerBase
 {
-    // [HttpPost]
-    // [ProducesResponseType(StatusCodes.Status200OK)]
-    // public ActionResult CreateUser(User userModel)
-    // {
-    //     Objects.UserStorage.CreateUser(userModel);
-    //     return Ok();
-    // }
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<User> Get(int id)
+    {
+        User user;
+        try
+        {
+            user = userStorage.Get(id);
+        }
+        catch (ArgumentException)
+        {
+            return NotFound();
+        }
+        return Ok(user);
+    }
 
-    // [HttpGet("{id}")]
-    // [ProducesResponseType(StatusCodes.Status200OK)]
-    // [ProducesResponseType(StatusCodes.Status404NotFound)]
-    // public ActionResult<User> GetUser(int id)
-    // {
-    //     UserStorage user;
-    //     try
-    //     {
-    //         user = Objects.UserStorage.GetUserById(id);
-    //     }
-    //     catch (ArgumentException)
-    //     {
-    //         return NotFound();
-    //     }
-    //     return Models.User.CreateFromUser(user);
-    // }
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public ActionResult<IEnumerable<User>> GetAll()
+    {
+        IEnumerable<User> users = userStorage.GetAll();
+        return Ok(users);
+    }
 
-    // [HttpGet]
-    // [ProducesResponseType(StatusCodes.Status200OK)]
-    // public ActionResult<User[]> GetAllUsers()
-    // {
-    //     UserStorage[] users = Objects.UserStorage.GetAllUsers();
-    //     List<User> userModels = [];
-    //     foreach (var user in users)
-    //     {
-    //         userModels.Add(Models.User.CreateFromUser(user));
-    //     }
-    //     return userModels.ToArray();
-    // }
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public ActionResult CreateUser(User user)
+    {
+        userStorage.Create(user);
+        return Created();
+    }
 
-    // [HttpPut("{id}")]
-    // [ProducesResponseType(StatusCodes.Status204NoContent)]
-    // [ProducesResponseType(StatusCodes.Status404NotFound)]
-    // public ActionResult EditUser(int id, User userModel)
-    // {
-    //     UserStorage user;
-    //     try
-    //     {
-    //         user = Objects.UserStorage.GetUserById(id);
-    //     }
-    //     catch (ArgumentException)
-    //     {
-    //         return NotFound();
-    //     }
-    //     user.Update(userModel);
-    //     return NoContent();
-    // }
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult Edit(int id, User user)
+    {
+        try
+        {
+            userStorage.Edit(id, user);
+        }
+        catch (ArgumentException)
+        {
+            return NotFound();
+        }
+        return NoContent();
+    }
 
-    // [HttpDelete("{id}")]
-    // [ProducesResponseType(StatusCodes.Status204NoContent)]
-    // [ProducesResponseType(StatusCodes.Status404NotFound)]
-    // public ActionResult DeleteUserById(int id)
-    // {
-    //     try
-    //     {
-    //         UserStorage user = Objects.UserStorage.GetUserById(id);
-    //         foreach (var canvasId in user.CreatedCanvases)
-    //         {
-    //             CanvasStorage.DeleteCanvasById(canvasId);
-    //         }
-    //         Objects.UserStorage.DeleteUserById(id);
-    //         return NoContent();
-    //     }
-    //     catch (ArgumentException)
-    //     {
-    //         return NotFound();
-    //     }
-    // }
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult Remove(int id)
+    {
+        try
+        {
+            userStorage.Remove(id);
+        }
+        catch (ArgumentException)
+        {
+            return NotFound();
+        }
+        return NoContent();
+    }
 }
