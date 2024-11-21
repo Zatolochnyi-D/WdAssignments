166d20972536616f23eef2e2f2ad82e270c655c5
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-11-21 17:25:35 +0200

    Created DB configurations for user and canvas.

---
+++ MyndMapper/Configurations/DB/CanvasConfiguration.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyndMapper.Entities;
+
+namespace MyndMapper.Configurations.DB;
+
+public class CanvasConfiguration : IEntityTypeConfiguration<Canvas>
+{
+    public void Configure(EntityTypeBuilder<Canvas> builder)
+    {
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.Name)
+            .IsRequired();
+
+        builder.Property(e => e.CreationDate)
+            .IsRequired();
+    }
+}
---
+++ MyndMapper/Configurations/DB/UserConfiguration.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyndMapper.Entities;
+
+namespace MyndMapper.Configurations.DB;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.Name)
+            .IsRequired();
+
+        builder.Property(e => e.Email)
+            .IsRequired();
+
+        builder.Property(e => e.Password)
+            .IsRequired();
+    }
+}
