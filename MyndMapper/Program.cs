dbb3b3a603f8f87ed211a27d7ce1e6734a227a89
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-11-21 12:19:03 +0200

    Removed custom code from Program class.

--- MyndMapper/Program.cs
+++ MyndMapper/Program.cs
@@ -1,39 +1,9 @@
-using MyndMapper.Models;
-using MyndMapper.Objects;
-
 namespace MyndMapper;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        UserStorage.CreateUser(new()
-        {
-            Name = "Athos",
-            Email = "firstOne@nonexistentmail.com",
-            Password = "123456",
-        });
-        UserStorage.CreateUser(new()
-        {
-            Name = "Porthos",
-            Email = "secondOne@nonexistentmail.com",
-            Password = "654321",
-        });
-        UserStorage.CreateUser(new()
-        {
-            Name = "Aramis",
-            Email = "thirdOne@nonexistentmail.com",
-            Password = "123321",
-        });
-        Canvas model = new()
-        {
-            Name = "NewCanvas",
-            CreationDate = DateTime.Now,
-        };
-        model.SetOwnerId(0);
-        CanvasStorage.CreateCanvas(model);
-        UserStorage.GetUserById(0).CreatedCanvases.Add(0);
-
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
