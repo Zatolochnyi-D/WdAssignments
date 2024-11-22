a9b456c7326e00abfc7c2006eeba2805689f94a1
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-11-22 17:09:27 +0200

    Created generic repository and it's interface.

---
+++ MyndMapper/Repositories/Generic/GenericRepository.cs
@@ -0,0 +1,33 @@
+
+namespace MyndMapper.Repositories.Generic;
+
+public class GenericRepository<TEntity, TKey>(DataModelContext context) : IGenericRepository<TEntity, TKey> where TEntity : class
+{
+    public virtual Task<TEntity?> GetAsync(TKey key)
+    {
+        return context.Set<TEntity>().FindAsync(key).AsTask();
+    }
+
+    public IQueryable<TEntity> GetAllAsync()
+    {
+        return context.Set<TEntity>();
+    }
+
+    public Task AddAsync(TEntity entity)
+    {
+        context.Set<TEntity>().Add(entity);
+        return context.SaveChangesAsync();
+    }
+
+    public Task EditAsync(TEntity entity)
+    {
+        context.Set<TEntity>().Update(entity);
+        return context.SaveChangesAsync();
+    }
+
+    public Task RemoveAsync(TEntity entity)
+    {
+        context.Set<TEntity>().Remove(entity);
+        return context.SaveChangesAsync();
+    }
+}
---
+++ MyndMapper/Repositories/Generic/IGenericRepository.cs
@@ -0,0 +1,14 @@
+namespace MyndMapper.Repositories.Generic;
+
+public interface IGenericRepository<TEntity, TKey> where TEntity : class
+{
+    public Task<TEntity?> GetAsync(TKey key);
+
+    public IQueryable<TEntity> GetAllAsync();
+
+    public Task AddAsync(TEntity entity);
+
+    public Task EditAsync(TEntity entity);
+
+    public Task RemoveAsync(TEntity entity);
+}
