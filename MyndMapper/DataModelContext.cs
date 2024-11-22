4ea2262f3bb8cc2acdf645bbf261ee01f1608f47
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-11-22 16:29:57 +0200

    Added to db context to use configurations from assembly.

--- MyndMapper/DataModelContext.cs
+++ MyndMapper/DataModelContext.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using MyndMapper.Entities;
 
@@ -8,5 +9,12 @@ public class DataModelContext : DbContext
     public DbSet<User> Users { get; set; } = null!;
     public DbSet<Canvas> Canvases { get; set; } = null!;
 
-    public DataModelContext(DbContextOptions<DataModelContext> options) : base(options) {} 
+    public DataModelContext(DbContextOptions<DataModelContext> options) : base(options) {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
 }
