3ca4ef017c0ee7642bb20390449edd20b4380d67
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-11-22 17:26:15 +0200

    Made all methods virtual and made context protected.

--- MyndMapper/Repositories/GenericRepository.cs
+++ MyndMapper/Repositories/GenericRepository.cs
@@ -4,29 +4,31 @@ namespace MyndMapper.Repositories;
 
 public class GenericRepository<TEntity, TKey>(DataModelContext context) : IGenericRepository<TEntity, TKey> where TEntity : class
 {
+    protected DataModelContext context = context;
+
     public virtual Task<TEntity?> GetAsync(TKey key)
     {
         return context.Set<TEntity>().FindAsync(key).AsTask();
     }
 
-    public IQueryable<TEntity> GetAllAsync()
+    public virtual IQueryable<TEntity> GetAllAsync()
     {
         return context.Set<TEntity>();
     }
 
-    public Task AddAsync(TEntity entity)
+    public virtual Task AddAsync(TEntity entity)
     {
         context.Set<TEntity>().Add(entity);
         return context.SaveChangesAsync();
     }
 
-    public Task EditAsync(TEntity entity)
+    public virtual Task EditAsync(TEntity entity)
     {
         context.Set<TEntity>().Update(entity);
         return context.SaveChangesAsync();
     }
 
-    public Task RemoveAsync(TEntity entity)
+    public virtual Task RemoveAsync(TEntity entity)
     {
         context.Set<TEntity>().Remove(entity);
         return context.SaveChangesAsync();
