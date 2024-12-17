206e5d604660b96e6e37144cfa1b9305d971e811
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-12-17 16:32:38 +0200

    Created client app with some JS tests.

---
+++ MyndMapperClient/app.js
@@ -0,0 +1,16 @@
+class CanvasGetDto {
+    fil;
+}
+
+class CanvasPostDto {
+
+}
+
+class CanvasPutDto {
+
+}
+
+console.log("hello world");
+gi = new CanvasGetDto()
+gi.fil = "string";
+console.log(gi.fil);
