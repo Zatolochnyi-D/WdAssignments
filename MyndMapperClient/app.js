ddf021b2bf2d66ca4d9db32bb9802c7aac30196a
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-12-17 16:51:03 +0200

    Tried fetching data from API.

--- MyndMapperClient/app.js
+++ MyndMapperClient/app.js
@@ -1,16 +1,35 @@
 class CanvasGetDto {
-    fil;
+    Id
+    Name
+    CreationDate
+    OwnerId
 }
 
 class CanvasPostDto {
-
+    OwnerId
+    Name
 }
 
 class CanvasPutDto {
-
+    Id
+    Name
 }
 
-console.log("hello world");
-gi = new CanvasGetDto()
-gi.fil = "string";
-console.log(gi.fil);
+async function getAll() {
+    let canvasGetAllUrl = "http://localhost:5110/canvases/get/all";
+    let response = await fetch(canvasGetAllUrl, {
+        method: "GET",
+        mode: "no-cors"
+    })
+    console.log(response);
+    if (response.ok) {
+        let data = await response.json();
+        console.log(data);
+        // let array = [];
+        // for (i = 0; i < data.lenght; i++) {
+        //     array.push(Object.assign(new CanvasGetDto, JSON.parse(data[i])))
+        // }
+    }
+}
+
+getAll()
