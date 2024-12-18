6b9cca124b3ab34cec3aa351f039a0441d29b667
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-12-18 13:07:30 +0200

    Made getAll work.

--- MyndMapperClient/app.js
+++ MyndMapperClient/app.js
@@ -1,35 +1,42 @@
+const ORIGIN = "http://localhost:5110/";
+const USER_ENDPOINT = "users/";
+const CANVAS_ENDPOINT = "canvases/";
+const GET_ALL = "get/all"
+
 class CanvasGetDto {
-    Id
-    Name
-    CreationDate
-    OwnerId
+    id
+    name
+    creationDate
+    ownerId
 }
 
 class CanvasPostDto {
-    OwnerId
-    Name
+    ownerId
+    name
 }
 
 class CanvasPutDto {
-    Id
-    Name
+    id
+    name
 }
+
+let button = document.getElementById("test-button");
+button.addEventListener("click", getAll)
 
 async function getAll() {
-    let canvasGetAllUrl = "http://localhost:5110/canvases/get/all";
-    let response = await fetch(canvasGetAllUrl, {
-        method: "GET",
-        mode: "no-cors"
-    })
+    let canvasGetAllUrl = ORIGIN + CANVAS_ENDPOINT + GET_ALL;
+    let response = await fetch(canvasGetAllUrl)
     console.log(response);
-    if (response.ok) {
-        let data = await response.json();
-        console.log(data);
-        // let array = [];
-        // for (i = 0; i < data.lenght; i++) {
-        //     array.push(Object.assign(new CanvasGetDto, JSON.parse(data[i])))
-        // }
-    }
+    let resp = await response.json()
+    console.log(resp);
+    let obj = Object.assign(new CanvasGetDto, resp[0]);
+    console.log(obj);
+    // if (response.ok) {
+    //     let data = await response.json();
+    //     console.log(data);
+    //     // let array = [];
+    //     // for (i = 0; i < data.lenght; i++) {
+    //     //     array.push(Object.assign(new CanvasGetDto, JSON.parse(data[i])))
+    //     // }
+    // }
 }
-
-getAll()
